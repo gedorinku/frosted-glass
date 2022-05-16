8fa9f7b6f93f684aefe232e77804a9a2e5f4946a
Ryota Egusa <egusa.ryota@gmail.com>
2022-05-16 23:25:53 +0900

    Remove unnecessary class

--- src/main/java/com/gedorinku/frostedglass/client/renderer/blockentity/FrostedGlassBlockEntityRenderer.java
+++ src/main/java/com/gedorinku/frostedglass/client/renderer/blockentity/FrostedGlassBlockEntityRenderer.java
@@ -15,25 +15,12 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
-import net.minecraft.client.renderer.block.model.BakedQuad;
-import net.minecraft.client.renderer.block.model.ItemOverrides;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.resources.model.BakedModel;
-import net.minecraft.client.resources.model.SimpleBakedModel;
-import net.minecraft.core.Direction;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.client.model.pipeline.IVertexConsumer;
-import org.jetbrains.annotations.Nullable;
 
 import java.nio.ByteBuffer;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 import static org.lwjgl.opengl.GL12.GL_BGRA;
@@ -94,129 +81,10 @@ public class FrostedGlassBlockEntityRenderer implements BlockEntityRenderer<Fros
 
         VertexConsumer vc = buffer.getBuffer(this.renderType());
         long seed = blockEntity.getBlockState().getSeed(blockEntity.getBlockPos());
-        var bakedModel = new SimpleBakedBlockModelWithoutAtlas((SimpleBakedModel) this.blockRenderer.getBlockModel(blockEntity.getBlockState()));
-        blockRenderer.getModelRenderer().tesselateBlock(level, bakedModel, blockEntity.getBlockState(), blockEntity.getBlockPos(), ps, vc, true, new Random(), seed, OverlayTexture.NO_OVERLAY, net.minecraftforge.client.model.data.EmptyModelData.INSTANCE);
+        blockRenderer.getModelRenderer().tesselateBlock(level, this.blockRenderer.getBlockModel(blockEntity.getBlockState()), blockEntity.getBlockState(), blockEntity.getBlockPos(), ps, vc, true, new Random(), seed, OverlayTexture.NO_OVERLAY, net.minecraftforge.client.model.data.EmptyModelData.INSTANCE);
     }
 
     protected RenderType renderType() {
         return RENDER_TYPE;
-    }
-
-    // TODO: int[] vertices を上書きするのは最悪っぽいので、良い感じに UV マッピングを読みこんで SimpleBakedModel を作るようにしたい
-    static class SimpleBakedBlockModelWithoutAtlas implements BakedModel {
-
-        private final SimpleBakedModel original;
-        private final Map<@Nullable Direction, List<BakedQuad>> quadsCache = new HashMap<>();
-
-        public SimpleBakedBlockModelWithoutAtlas(SimpleBakedModel original) {
-            this.original = original;
-        }
-
-        @Override
-        public List<BakedQuad> getQuads(@Nullable BlockState blockState, @Nullable Direction direction, Random random) {
-            // net.minecraft.client.resources.model.SimpleBakedModel#getQuads のロジックと整合性が取られている必要がある。
-            if (quadsCache.get(direction) != null) {
-                return quadsCache.get(direction);
-            }
-
-            var quads = original.getQuads(blockState, direction, random).stream().map(bakedQuad -> (BakedQuad) new BlockBakedQuad(bakedQuad)).toList();
-            quadsCache.put(direction, quads);
-
-            return quads;
-        }
-
-        @Override
-        public boolean useAmbientOcclusion() {
-            return original.useAmbientOcclusion();
-        }
-
-        @Override
-        public boolean isGui3d() {
-            return original.isGui3d();
-        }
-
-        @Override
-        public boolean usesBlockLight() {
-            return original.usesBlockLight();
-        }
-
-        @Override
-        public boolean isCustomRenderer() {
-            return original.isCustomRenderer();
-        }
-
-        @Override
-        public TextureAtlasSprite getParticleIcon() {
-            return original.getParticleIcon();
-        }
-
-        @Override
-        public ItemOverrides getOverrides() {
-            return original.getOverrides();
-        }
-    }
-
-    static class BlockBakedQuad extends BakedQuad {
-        private final BakedQuad original;
-        private final int[] vertices;
-
-        public BlockBakedQuad(BakedQuad original) {
-            super(null, 0, null, null, false);
-
-            this.original = original;
-
-            final int VERTEX_COUNT = 4;
-            var byteBuffer = ByteBuffer.allocate(original.getVertices().length * Integer.BYTES);
-            var intBuffer = byteBuffer.asIntBuffer();
-            intBuffer.put(original.getVertices());
-
-            final int U_LOCATION = 16;
-            final int V_LOCATION = 20;
-            final var U = new float[]{0.0f, 0.0f, 1.0f, 1.0f};
-            final var V = new float[]{1.0f, 0.0f, 0.0f, 1.0f};
-            for (int vertexIndex = 0; vertexIndex < 4; vertexIndex++) {
-                final int offset = vertexIndex * byteBuffer.capacity() / VERTEX_COUNT;
-                byteBuffer.putFloat(offset + U_LOCATION, U[vertexIndex]);
-                byteBuffer.putFloat(offset + V_LOCATION, V[vertexIndex]);
-            }
-
-            vertices = new int[original.getVertices().length];
-            intBuffer.get(0, vertices);
-        }
-
-        @Override
-        public TextureAtlasSprite getSprite() {
-            return original.getSprite();
-        }
-
-        @Override
-        public int[] getVertices() {
-            return vertices;
-        }
-
-        @Override
-        public boolean isTinted() {
-            return original.isTinted();
-        }
-
-        @Override
-        public int getTintIndex() {
-            return original.getTintIndex();
-        }
-
-        @Override
-        public Direction getDirection() {
-            return original.getDirection();
-        }
-
-        @Override
-        public void pipe(IVertexConsumer consumer) {
-            original.pipe(consumer);
-        }
-
-        @Override
-        public boolean isShade() {
-            return original.isShade();
-        }
     }
 }
